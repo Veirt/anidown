e2da7a9d6f2fc26bb8ddec808f2ee77a285645b3
Veirt <veirt@duck.com>
2024-05-04 23:50:23 +0800

    feat: initial commit

---
+++ web/src/lib/components/NavBar.svelte
@@ -0,0 +1,10 @@
+<div class="navbar bg-base-100">
+    <div class="navbar-start">
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <a class="text-xl btn btn-ghost">AniDown</a>
+
+        <ul class="px-1 menu menu-horizontal">
+            <li><a class="font-bold" href="/">Home</a></li>
+        </ul>
+    </div>
+</div>
